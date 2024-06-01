0b43dfce5f8e55c1cd43b0840229456f2ea91303
8ctopotamus <zylo.codes@gmail.com>
2024-06-01 18:25:00 -0500

    pointers

--- pointers.c
+++ pointers.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+struct employee_t {
+  int id;
+  int income;
+  bool staff;
+};
+
+void initialize_employee(struct employee_t *e) {
+  e->id = 0;
+  e->income = 0;
+  e->staff = false;
+}
 
 int main() {
   
   int x = 3;
-  
   int *pX = &x;
 
   printf("%d\n", *pX);
   printf("%p\n", pX);
+
+  struct employee_t Ralph;
+  initialize_employee(&Ralph);
+  printf("Ralph's income: %d\n", Ralph.income);
 
   return 0;
 }
