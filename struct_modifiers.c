32f3efc4d3742fb470d0c3736618ee2ded10fc03
8ctopotamus <zylo.codes@gmail.com>
2024-06-01 17:22:42 -0500

    unions, struct modifiers

---
+++ struct_modifiers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX_EMPLOYEES 100
+
+// make sure no additional optimizations
+// so memory is consistent between machines
+__attribute__((__packed__)) struct employee_t {
+	int id;
+	char firstname[64];
+	char lastname[64];
+	float income;
+	bool ismanager;
+};
+
+int main() {
+	struct employee_t Fred;
+	
+	printf("Size of employee: %lu\n", sizeof(Fred));
+
+	return 0;
+}
