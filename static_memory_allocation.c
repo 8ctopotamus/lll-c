f440e67fd654f8dd383f3e66bcef2b2375799a84
8ctopotamus <zylo.codes@gmail.com>
2024-06-02 08:35:51 -0500

    static memory allocation

--- static_memory_allocation.c
+++ static_memory_allocation.c
@@ -8,30 +8,34 @@ struct employee_t {
   bool staff;
 };
 
-void initialize_employee(struct employee_t *e) {
-  e->id = 0;
+int initialize_employee(struct employee_t *e) {
+	static int numEmployees = 0;
+	numEmployees++;
+
+	e->id = numEmployees;
   e->income = 0;
   e->staff = false;
-  return;
+
+	return numEmployees;
 }
 
 int main() {
   
   int n = 4;
 
-  struct employee_t *employees = malloc(sizeof(struct employee_t) * n);
+	struct employee_t *employees = malloc(sizeof(struct employee_t) * n);
+	if (employees == NULL) {
+		printf("The allocator failed\n");
+		return -1;
+	}
 
-  if (employees == NULL) {
-    printf("The allocator failed\n");
-    return -1;
-  }
+	for (int i = 0; i < n; i++) {
+		int id = initialize_employee(&employees[i]);
+		printf("New employee, id: %d\n", id);
+	}
 
-  initialize_employee(&employees[0]);
-
-  printf("%d\n", employees[0].income);
-
-  free(employees);
-  employees = NULL;
+	free(employees);
+	employees = NULL;
 
   return 0;
 }
