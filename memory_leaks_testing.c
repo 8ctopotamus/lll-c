8d890550b5236d6f54bf74ef17e95bc72b6cbf54
8ctopotamus <zylo.codes@gmail.com>
2024-06-02 10:30:57 -0500

    testing for memory leaks with Valgrind

---
+++ memory_leaks_testing.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct employee_t {
+  int id;
+  int income;
+  bool staff;
+};
+
+int initialize_employee(struct employee_t *e) {
+	static int numEmployees = 0;
+	numEmployees++;
+
+	e->id = numEmployees;
+  e->income = 0;
+  e->staff = false;
+
+	return numEmployees;
+}
+
+int main() {
+  
+  int n = 4;
+
+	struct employee_t *employees = malloc(sizeof(struct employee_t) * n);
+	if (employees == NULL) {
+		printf("The allocator failed\n");
+		return -1;
+	}
+
+	for (int i = 0; i < n; i++) {
+		int id = initialize_employee(&employees[i]);
+		printf("New employee, id: %d\n", id);
+	}
+
+	// comment in/out the free line and test with valgrind to see the difference
+	// free(employees);
+	employees = NULL;
+
+  return 0;
+}
--- readme.md
+++ readme.md
@@ -9,3 +9,21 @@ Let's compile them to the `./bin` folder for now.
 ## Run
 
 `./bin/<out_file>`
+
+## Testing for Memory Leaks with Valgrind
+
+1) Compile with `-g` flag
+
+`gcc <source_file>.c -g -o ./bin/<out_file>`
+
+2) Inspect result - you'll see a large number
+
+`wc -c <out_file>`
+
+3) Install Valgrind
+
+`sudo apt install valgrind`
+
+4) Use Valgrind
+
+`valgrind --leak-check=full ./<out_file> -s`
